8a2ccbedb0c0893f7f4220d6b67c66014fea4cb1
chenqiuling <249370878@qq.com>
2024-10-18 17:22:27 +0800

    Update Page1.tsx

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,12 +1,22 @@
-import { Form, Input, Select, Table, Tabs } from "antd"
+import { Dropdown, Form, Input, Menu, Select, Table, Tabs } from "antd"
+import { DownOutlined } from "@ant-design/icons"
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useDispatch } from "react-redux"
 import { initLayoutTab } from "../stores/layoutService"
 
+const dropdownMenuData = [
+  { label: "选项一", key: "option1" },
+  { label: "选项二", key: "option2" },
+]
 const { Option } = Select
 const Page1 = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
+  const [menu, setMenu] = useState<{
+    label: string
+    key: string
+  }>()
 
   const handleDetailLink = (val: string) => {
     const encParams = btoa(JSON.stringify({ userId: val }))
@@ -19,83 +29,98 @@ const Page1 = () => {
       })
     )
   }
-
   return (
-    <>
-      <Tabs onChange={(ack) => {}}>
-        <Tabs.TabPane tab="页面1的tab1" key="/tab1">
-          <Form>
-            <Form.Item label="tab1字段1" name="username1">
-              <Input />
-            </Form.Item>
-            <Form.Item label="tab1字段2" name="gender1">
-              <Select allowClear>
-                <Option value="male">male2</Option>
-                <Option value="female">female2</Option>
-                <Option value="other">other2</Option>
-              </Select>
-            </Form.Item>
-          </Form>
+    <Tabs>
+      <Tabs.TabPane tab="页面1的tab1" key="/tab1">
+        <Dropdown
+          trigger={["click"]}
+          overlay={
+            <Menu
+              onClick={(info) => {
+                setMenu(dropdownMenuData.find((m) => m.key === info.key))
+              }}
+            >
+              {dropdownMenuData.map((menu) => (
+                <Menu.Item key={menu.key}>{menu.label}</Menu.Item>
+              ))}
+            </Menu>
+          }
+        >
+          <span style={{ cursor: "pointer" }}>
+            {menu?.label || "点击选择"} <DownOutlined />
+          </span>
+        </Dropdown>
+        <Form>
+          <Form.Item label="tab1字段1" name="username1">
+            <Input />
+          </Form.Item>
+          <Form.Item label="tab1字段2" name="gender1">
+            <Select allowClear>
+              <Option value="male">male2</Option>
+              <Option value="female">female2</Option>
+              <Option value="other">other2</Option>
+            </Select>
+          </Form.Item>
+        </Form>
 
-          <Table
-            rowKey="userId"
-            columns={[
-              {
-                dataIndex: "userId",
-                title: "用户编码",
-                render: (val) => (
-                  <span
-                    style={{ color: "blue", cursor: "pointer" }}
-                    onClick={() => handleDetailLink(val)}
-                  >
-                    {val}
-                  </span>
-                ),
-              },
-              { dataIndex: "userName", title: "姓名" },
-            ]}
-            dataSource={[
-              { userId: "123", userName: "甲xx" },
-              { userId: "456", userName: "乙xx" },
-            ]}
-          />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="页面1的tab2" key="/tab2">
-          <Form>
-            <Form.Item label="tab2字段1" name="username2">
-              <Input />
-            </Form.Item>
-            <Form.Item label="tab2字段2" name="gender2">
-              <Select allowClear>
-                <Option value="male">male2</Option>
-                <Option value="female">female2</Option>
-                <Option value="other">other2</Option>
-              </Select>
-            </Form.Item>
-          </Form>
+        <Table
+          rowKey="userId"
+          columns={[
+            {
+              dataIndex: "userId",
+              title: "用户编码",
+              render: (val) => (
+                <span
+                  style={{ color: "blue", cursor: "pointer" }}
+                  onClick={() => handleDetailLink(val)}
+                >
+                  {val}
+                </span>
+              ),
+            },
+            { dataIndex: "userName", title: "姓名" },
+          ]}
+          dataSource={[
+            { userId: "123", userName: "甲xx" },
+            { userId: "456", userName: "乙xx" },
+          ]}
+        />
+      </Tabs.TabPane>
+      <Tabs.TabPane tab="页面1的tab2" key="/tab2">
+        <Form>
+          <Form.Item label="tab2字段1" name="username2">
+            <Input />
+          </Form.Item>
+          <Form.Item label="tab2字段2" name="gender2">
+            <Select allowClear>
+              <Option value="male">male2</Option>
+              <Option value="female">female2</Option>
+              <Option value="other">other2</Option>
+            </Select>
+          </Form.Item>
+        </Form>
 
-          <Table
-            rowKey="userId"
-            columns={[
-              {
-                dataIndex: "userId",
-                title: "用户编码",
-                render: (val) => (
-                  <span
-                    style={{ color: "blue", cursor: "pointer" }}
-                    onClick={() => handleDetailLink(val)}
-                  >
-                    {val}
-                  </span>
-                ),
-              },
-              { dataIndex: "userName", title: "姓名" },
-            ]}
-            dataSource={[{ userId: "789", userName: "丙xx" }]}
-          />
-        </Tabs.TabPane>
-      </Tabs>
-    </>
+        <Table
+          rowKey="userId"
+          columns={[
+            {
+              dataIndex: "userId",
+              title: "用户编码",
+              render: (val) => (
+                <span
+                  style={{ color: "blue", cursor: "pointer" }}
+                  onClick={() => handleDetailLink(val)}
+                >
+                  {val}
+                </span>
+              ),
+            },
+            { dataIndex: "userName", title: "姓名" },
+          ]}
+          dataSource={[{ userId: "789", userName: "丙xx" }]}
+        />
+      </Tabs.TabPane>
+    </Tabs>
   )
 }
 
